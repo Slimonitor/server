20385bc16b25c5a68402a79e769ec8e455a83ee1
Robin Tail <robin_tail@me.com>
2018-12-25 07:03:57 +0100

    MongoDb connection using Mongoose

--- config.default.js
+++ config.default.js
@@ -1,3 +1,9 @@
 module.exports = {
-    port: 80
+    port: 80,
+    mongoDbUrl: 'mongodb://<user>:<password>@<host>:<port>/<database>',
+    mongooseOptions: {
+        useNewUrlParser: true,
+        keepAlive: 300000,
+        connectTimeoutMS: 30000
+    }
 };
--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 const debug = require('debug')('slimonitor:index');
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const util = require('util');
 
 const app = express();
 const config = require('../config.js');
 const handlers = require('./handlers.js');
 
+mongoose.Promise = global.Promise;
 app.use(bodyParser.json());
 app.post('/host/data', handlers.collectIncomingData);
 app.use(handlers.catchError);
 
-app.listen(config.port, () => {
+
+mongoose.connect(config.mongoDbUrl, config.mongooseOptions).then(() => {
+    debug('Connected to MongoDb');
+    return util.promisify(app.listen)(config.port);
+}).then(() => {
     debug('Start serving on port', config.port);
+}).catch(err => {
+    debug(err.toString());
 });
 
