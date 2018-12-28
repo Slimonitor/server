ea9a8613644543c6bfae85b8f4d2cf80467d1c66
Robin Tail <robin_tail@me.com>
2018-12-28 02:23:21 +0100

    Initial webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+
+module.exports = {
+    entry: ['./components/entry.jsx'],
+    output: {
+        path: __dirname + '/dist',
+        filename: 'entry.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                loaders: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: [
+                                autoprefixer({
+                                    browsers: ['last 2 versions'],
+                                    cascade: false
+                                })
+                            ]
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.json$/,
+                exclude: /node_modules/,
+                loader: 'json-loader'
+            },
+            {
+                test: /\.svg$/,
+                exclude: /node_modules/,
+                loader: 'url-loader'
+            }
+        ]
+    },
+    mode: process.env.NODE_ENV !== 'production' ? 'production' : 'development',
+    optimization: {
+        minimize: process.env.NODE_ENV !== 'production'
+    }
+};
